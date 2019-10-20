38c7e8b55f220fcfcb26cadb291818218e80766b
Adereglazov <sanya95@yandex.ru>
2019-10-20 23:50:22 +0300

    Pixel Perfect после всех исправлений - 2

--- js/map.js
+++ js/map.js
@@ -1,18 +1,18 @@
 function initMap() {
   var coordinates = {lat: 59.938802, lng: 30.323283},
-    markerImage = "img/map-marker.png",
+      markerImage = "img/map-marker.png",
 
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: coordinates,
-      disableDefaultUI: true,
-      scrollwheel: false,
-      zoom: 17
-    }),
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        zoom: 17
+      }),
 
-    marker = new google.maps.Marker({
-      position: coordinates,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      icon: markerImage
-    });
+      marker = new google.maps.Marker({
+        position: coordinates,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon: markerImage
+      });
 }
